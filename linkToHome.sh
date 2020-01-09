06dba20264d64a46d5fbe3fcd6bf910411cec11f
Erik on Endeavour <erik.falor@usu.edu>
2020-01-09 10:42:31 -0700

    add dircolors; customized for lecture notes

--- linkToHome.sh
+++ linkToHome.sh
@@ -71,7 +71,7 @@ if [ 0"$DRYRUN" != "0" ]; then
 	echo
 fi
 
-PAYLOAD_FILES="ackrc csirc gitconfig inputrc Xdefaults xprofile"
+PAYLOAD_FILES="ackrc csirc dircolors gitconfig inputrc Xdefaults xprofile"
 
 if [ 0"$1" = 0"-r" ]; then
 	# Clean up old symlinks
